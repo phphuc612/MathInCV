15cf5d0d810aea1e26d8f13e1e5b4f7de06ba1ac
phphuc612 <phphuc612@gmail.com>
2023-11-12 23:04:41 +0700

    update fractal transformation schema

--- src/schemas/fractal_transformation.py
+++ src/schemas/fractal_transformation.py
@@ -1,13 +1,42 @@
 import pydantic
+from typing import Any
 
 from pydantic import BaseModel
 from pydantic import *
 
+from src.utils.validation_helpers import is_evaluable
+
 
 class FractalTransformation(BaseModel):
+    """
+    A class representing a fractal transformation.
+
+    Attributes:
+    -----------
+    _probability : float
+        The probability of the transformation being applied.
+    _str_transformation : str
+        The string representation of the transformation.
+    """
+
     _probability: PrivateAttr[float]
     _str_transformation: PrivateAttr[str]
 
+    class FractalTransformation:
+        def __init__(self, str_transformation: str):
+            self._str_transformation = str_transformation
+
+        def transform(self, **kwargs) -> Any:
+            """
+            Applies the fractal transformation to the given input.
+
+            Args:
+                **kwargs: Keyword arguments representing the input values.
+
+            Returns:
+                The result of applying the fractal transformation to the input.
+            """
+            return eval(self._str_transformation, kwargs)
 
     @validator('_probability')
     def validate_probability(cls, v):
@@ -19,4 +48,6 @@ class FractalTransformation(BaseModel):
     def validate_str_transformation(cls, v):
         if v == '':
             raise ValueError(f'Transformation cannot be empty.')
+        if not is_evaluable(v):
+            raise ValueError(f'Transformation is not evaluable.')
         return v
---
+++ src/utils/validation_helpers.py
@@ -0,0 +1,9 @@
+import logging
+
+def is_evaluable(cmd: str) -> bool:
+    try:
+        compile(cmd, "", "eval")
+        return True
+    except Exception as e:
+        logging.error(f'Error parsing command: {e}')
+        return False        
