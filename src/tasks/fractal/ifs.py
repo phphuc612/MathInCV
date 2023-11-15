b6dcb7655651813d462312bd89e36cf1f710eaec
phphuc612 <phphuc612@gmail.com>
2023-11-15 07:41:04 +0700

    update test

--- src/tasks/fractal/ifs.py
+++ src/tasks/fractal/ifs.py
@@ -6,6 +6,7 @@ from __future__ import division
 from typing import List, Tuple
 
 from numpy import random
+from PIL import Image, ImageDraw
 
 from src.schemas.fractal_transformation import FractalTransformation
 
@@ -23,103 +24,34 @@ def generate_points(
     for choice in choices:
         point = transformations[choice].transform(x = point[0], y = point[1])
         points.append(point)
-    
+
     return points
 
 
-# def process_file(transformations, width, height, iterations=1, outputfile="out.png"):
+def generate_fractal(transformations: List[FractalTransformation], width: int, height: int, outputfile: str):
+    points = generate_points(transformations)
 
-#     probability_join = sum(t[0] for t in transformations)
+    # find out image limits determine scaling and translating
+    min_x = min(points, key=lambda p: p[0])[0]
+    max_x = max(points, key=lambda p: p[0])[0]
+    min_y = min(points, key=lambda p: p[1])[1]
+    max_y = max(points, key=lambda p: p[1])[1]
+    p_width = max_x - min_x
+    p_height = max_y - min_y
 
-#     points = set([(0, 0)])
+    width_scale = width / p_width
+    height_scale = height / p_height
+    scale = min(width_scale, height_scale)
 
-#     # for each iteration
-#     for i in range(iterations):
-#         new_points = set()
+    # create new image
+    image = Image.new("RGB", (width, height))
+    draw = ImageDraw.Draw(image)
 
-#         # for each point
-#         for point in points:
+    # plot points
+    for point in points:
+        x = (point[0] - min_x) * scale
+        y = height - (point[1] - min_y) * scale
+        draw.point((x, y))
 
-#             # decide on which transformation to apply
-#             rnd = uniform(0, probability_join)
-#             p_sum = 0
-#             for probability, function in transformations:
-#                 p_sum += probability
-#                 if rnd <= p_sum:
-#                     new_points.add(function(*point))
-#                     break
-
-#         points.update(new_points)
-#         print(i)
-
-#     # find out image limits determine scaling and translating
-#     min_x = min(points, key=lambda p: p[0])[0]
-#     max_x = max(points, key=lambda p: p[0])[0]
-#     min_y = min(points, key=lambda p: p[1])[1]
-#     max_y = max(points, key=lambda p: p[1])[1]
-#     p_width = max_x - min_x
-#     p_height = max_y - min_y
-
-#     width_scale = width / p_width
-#     height_scale = height / p_height
-#     scale = min(width_scale, height_scale)
-
-#     # create new image
-#     image = Image.new("RGB", (width, height))
-#     draw = ImageDraw.Draw(image)
-
-#     # plot points
-#     for point in points:
-#         x = (point[0] - min_x) * scale
-#         y = height - (point[1] - min_y) * scale
-#         draw.point((x, y))
-
-#     # save image file
-#     image.save(outputfile, "PNG")
-
-
-# def parse(filename):
-#     with open(filename) as f:
-#         definition = load(f)
-
-#     # check for errors
-#     if "width" not in definition:
-#         raise ValueError('"width" parameter missing')
-#     if "height" not in definition:
-#         raise ValueError('"height" parameter missing')
-#     if "iterations" not in definition:
-#         raise ValueError('"iterations" parameter missing')
-#     if "transformations" not in definition:
-#         raise ValueError('"transformations" parameter missing')
-
-#     def make_t_function(expression):
-#         return lambda x, y: eval(expression, {"x": x, "y": y})
-
-#     definition["transformations"] = [
-#         (float(probability), make_t_function(expression))
-#         for probability, expression in definition["transformations"]
-#     ]
-
-#     return definition
-
-
-# if __name__ == "__main__":
-
-#     import sys
-
-#     # if there is one argument and it's not "-"
-#     if len(sys.argv) > 1 and sys.argv[1] != "-":
-#         # process each filename in input
-#         for filename in sys.argv[1:]:
-#             result = parse(filename)
-#             process_file(
-#                 result["transformations"],
-#                 result["width"],
-#                 result["height"],
-#                 result["iterations"],
-#                 filename.split(".")[0] + ".png",
-#             )
-#     else:
-#         # read contents from stdin
-#         eval(sys.stdin.read())
-#         process_file(transformation, width, height, iterations)
+    # save image file
+    image.save(outputfile, "PNG")
